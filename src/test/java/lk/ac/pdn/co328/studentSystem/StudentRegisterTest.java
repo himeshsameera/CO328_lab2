6d993d300714906b2cd6912b1aa3245b1c71ef3c
gunarathnemdd <gunarathnemdd@gmail.com>
2016-07-11 21:21:11 +0530

    Changed ArtifactID to E12103_lab2 in pom.xml

--- src/test/java/lk/ac/pdn/co328/studentSystem/StudentRegisterTest.java
+++ src/test/java/lk/ac/pdn/co328/studentSystem/StudentRegisterTest.java
@@ -6,10 +6,7 @@ public class StudentRegisterTest {
     StudentRegister register;
 
     @Before
-    public void setupTest()
-    {
-        System.out.println("A new test is starting.");
-    }
+    public void setupTest() { System.out.println("A new test is starting.");}
 
     @After
     public void finishTest()
@@ -52,7 +49,20 @@ public class StudentRegisterTest {
     public void testAddStudentTwice()
    {
        // Implement your test code here. Adding a student with same registration number twice should generate an exception.
-       Assert.fail("Test case is not yet implemented for adding student twice. So it is set to fail always");
+       //Assert.fail("Test case is not yet implemented for adding student twice. So it is set to fail always");
+       register = new StudentRegister();
+       String expected = "StudentID already exists in the register";
+       String actual = "Not initialized a message yet";
+       try{
+           register.addStudent(new Student(5, "fawzan", "mohomad"));
+           register.addStudent(new Student(5, "dhanuka", "dilsan"));
+       }
+       catch (Exception e){
+           actual = e.getMessage();
+       }
+       System.out.println("Testing add student twice method");
+
+       Assert.assertEquals(expected,actual);
    }
 
     @Test
