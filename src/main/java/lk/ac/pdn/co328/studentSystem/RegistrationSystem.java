354920a4c0d0f7dc561c74d28030e1a0e4ecd984
Udeni Balasooriya <e12026@ce.pdn.ac.lk>
2016-07-19 00:17:24 +0530

    Implemented the features.

--- src/main/java/lk/ac/pdn/co328/studentSystem/RegistrationSystem.java
+++ src/main/java/lk/ac/pdn/co328/studentSystem/RegistrationSystem.java
@@ -1,10 +1,10 @@
 package lk.ac.pdn.co328.studentSystem;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class RegistrationSystem
 {
-    static Scanner stdin = new Scanner(System.in);;
+    static Scanner stdin = new Scanner(System.in);
     static StudentRegister register = new StudentRegister();
     public static void main(String[] args)
     {
@@ -21,13 +21,21 @@ public class RegistrationSystem
                         return;
                     case 1:
                         addStudent();
-
                         break;
                     case 2:
                         removeStudent();
                         break;
                     case 3:
                         viewStudent();
+                        break;
+                    case 4:
+                        findStudentsByName();
+                        break;
+                    case 5:
+                        cleanStudentRegister();
+                        break;
+                    case 6:
+                        getAllRegistrationNumbers();
                         break;
                     default:
                         System.out.println("Please select an available feature");
@@ -40,6 +48,8 @@ public class RegistrationSystem
         }
     }
 
+
+
     private static void printSelection()
     {
         System.out.println("Select an option : ");
@@ -47,9 +57,9 @@ public class RegistrationSystem
         System.out.println("  1 - Add a student");
         System.out.println("  2 - Remove a student");
         System.out.println("  3 - Search a student by registration number");
-        System.out.println("  4 - Search students by name [Feature implementation ongoing]");
-        System.out.println("  5 - Clean student register [Feature implementation ongoing]");
-        System.out.println("  6 - Get all the students [Feature implementation ongoing]");
+        System.out.println("  4 - Search students by name ");
+        System.out.println("  5 - Clean student register ");
+        System.out.println("  6 - Get all the students ");
         System.out.println("  7 - Save to file/DB [Feature implementation ongoing]");
         System.out.println("  8 - Load from file/DB [Feature implementation ongoing]");
     }
@@ -88,22 +98,16 @@ public class RegistrationSystem
     {
         int regNo = 0;
         System.out.println("Enter reg number");
-        try
-        {
+        try {
             regNo = Integer.parseInt(stdin.nextLine());
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("Invalid inputs.");
             return;
         }
 
-        try
-        {
+        try {
             register.removeStudent(regNo);
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println("Error in removing student : " + ex.getMessage());
         }
     }
@@ -142,5 +146,49 @@ public class RegistrationSystem
         {
             System.out.println("Student not found");
         }
+    }
+
+    private static void findStudentsByName() {
+
+        ArrayList<Student> students = new ArrayList<Student>();
+        System.out.println("Enter the name:");
+        String name = stdin.nextLine();
+
+        try {
+            students = register.findStudentsByName(name);
+        }catch (Exception ex) {
+            System.out.println("Errors happening when searching students by name: " + ex.getMessage());
+            return;
+        }
+
+        for (Student student: students) {
+            System.out.println("Student ID : " + student.getId());
+        }
+    }
+
+    private static void cleanStudentRegister() {
+        try {
+            register.reset();
+        } catch (Exception ex) {
+            System.out.println("Error in removing data from the register: " + ex.getMessage());
+            return;
+        }
+    }
+
+    private static void getAllRegistrationNumbers() {
+
+        ArrayList<Integer> allregNums = new ArrayList<Integer>();
+        try {
+            allregNums = register.getAllRegistrationNumbers();
+            //System.out.println(" List of reg_nums: ");
+            // let us print all the elements available in list
+            for (Integer number : allregNums) {
+                System.out.println(number);
+            }
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+            return;
+        }
+
     }
 }
