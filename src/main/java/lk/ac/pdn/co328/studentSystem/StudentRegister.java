7db8eac7fd5e4646bb4cc12f5fd7581c534e9721
SanduniKanishka <e12040@ce.pdn.ac.lk>
2016-07-24 12:55:57 +0530

    StudentRegister.java

--- src/main/java/lk/ac/pdn/co328/studentSystem/StudentRegister.java
+++ src/main/java/lk/ac/pdn/co328/studentSystem/StudentRegister.java
@@ -45,7 +45,7 @@ public class StudentRegister
     // Cleans all the data from the student register
     public void reset()
     {
-        studentList = null;
+        studentList.clear();
     }
 
     // Finds all the students that has the given name as a part of their name.
@@ -56,13 +56,13 @@ public class StudentRegister
         {
             if(studentList.get(i).getFirstName().contains(name))
             {
-                studentList.add(studentList.get(i));
+                students.add(studentList.get(i));
             }
 			
 			
             if(studentList.get(i).getLastName().contains(name))
             {
-                studentList.add(studentList.get(i));
+                students.add(studentList.get(i));
             }
         }
         return students;
@@ -72,10 +72,13 @@ public class StudentRegister
     public ArrayList<Integer> getAllRegistrationNumbers()
     {
         ArrayList<Integer> regNumbers = new ArrayList<Integer>();
-        for (Student student: studentList)
-        {
-            regNumbers.add(student.getId());
-        }
+        try{
+            for (Student student: studentList)
+            {
+                regNumbers.add(student.getId());
+            }
+        }catch(Exception e){}
+
         return  regNumbers;
     }
 }
