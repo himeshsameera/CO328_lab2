0e7c59ad56f2363d9d89b6ff5ea26363980df4bb
Chamith Shanaka <e12379@ce.pdn.ac.lk>
2016-07-25 23:37:00 +0530

    fixed the test case

--- src/test/java/lk/ac/pdn/co328/studentSystem/StudentRegisterTest.java
+++ src/test/java/lk/ac/pdn/co328/studentSystem/StudentRegisterTest.java
@@ -8,7 +8,19 @@ public class StudentRegisterTest {
     @Before
     public void setupTest()
     {
+
         System.out.println("A new test is starting.");
+        register = new StudentRegister();
+        try
+        {
+            register.addStudent(new Student(1, "ruwan", "tharaka"));
+            register.addStudent(new Student(2, "nimal", "kumara"));
+            register.addStudent(new Student(5, "gayan", "chamara"));
+        }
+        catch (Exception ex)
+        {
+            Assert.fail("Add student failed");
+        }
     }
 
     @After
@@ -51,24 +63,23 @@ public class StudentRegisterTest {
    @Test
     public void testAddStudentTwice()
    {
-       // Implement your test code here. Adding a student with same registration number twice should generate an exception.
-       Assert.fail("Test case is not yet implemented for adding student twice. So it is set to fail always");
+       register = new StudentRegister();
+       try
+       {
+           register.addStudent(new Student(1, "saman", "wiki"));
+           register.addStudent(new Student(1, "kamal", "chami"));
+           Assert.fail("Adding student with a same registration number twice.");
+
+       }
+       catch (Exception e) {
+
+       }
    }
 
     @Test
     public void testRemoveStudent()
     {
-        register = new StudentRegister();
-        try
-        {
-            register.addStudent(new Student(2, "nimal", "kumara"));
-            register.addStudent(new Student(1, "ruwan", "tharaka"));
-            register.addStudent(new Student(5, "gayan", "chamara"));
-        }
-        catch (Exception ex)
-        {
-            Assert.fail("Add student failed");
-        }
+        
         register.removeStudent(1);
         Student student = register.findStudent(1);
         Assert.assertNull("student was not removed",student);
@@ -77,17 +88,7 @@ public class StudentRegisterTest {
     @Test
     public void testGetRegNumbers()
     {
-        register = new StudentRegister();
-        try
-        {
-            register.addStudent(new Student(1, "ruwan", "tharaka"));
-            register.addStudent(new Student(2, "nimal", "kumara"));
-            register.addStudent(new Student(5, "gayan", "chamara"));
-        }
-        catch (Exception ex)
-        {
-            Assert.fail("Adding student failed");
-        }
+
         ArrayList<Integer> numbers = register.getAllRegistrationNumbers();
         ArrayList<Integer> expected = new ArrayList<Integer>();
         expected.add(1);
