8f77cb821b0093d0d4efb05743c9809851cb0582
pranavan15 <Pranavan15@yahoo.com>
2016-08-08 18:47:32 +0530

    Lab 2 done

--- src/main/java/lk/ac/pdn/co328/studentSystem/StudentRegister.java
+++ src/main/java/lk/ac/pdn/co328/studentSystem/StudentRegister.java
@@ -1,81 +1,73 @@
 package lk.ac.pdn.co328.studentSystem;
+
 import java.util.ArrayList;
 
-public class StudentRegister
-{
+public class StudentRegister {
+    private static Exception duplicateStudentIdException;
+
     private ArrayList<Student> studentList = new ArrayList<Student>();
+
+    public static Exception getDuplicateStudentIdException() {
+        return duplicateStudentIdException;
+    }
 
     // Adds a new student to the system
     public void addStudent(Student st) throws Exception {
-        for (Student student:studentList)
-        {
-            if(student.getId() == st.getId())
-            {
-                throw new Exception("StudentID already exists in the register");
+        for (Student student : studentList) {
+            if (student.getId() == st.getId()) {
+                duplicateStudentIdException = new Exception("StudentID already exists in the register");
+                throw duplicateStudentIdException;
             }
         }
         studentList.add(st);
     }
 
     // Remove a student from the system
-    public void removeStudent(int regNo)
-    {
-        for (int i = 0; i<studentList.size(); i++)
-        {
-            if(studentList.get(i).getId() == regNo)
-            {
+    public void removeStudent(int regNo) {
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getId() == regNo) {
                 studentList.remove(i);
             }
         }
     }
 
     //Finds the student with the given registration number
-    public Student findStudent(int regNo)
-    {
-        for (int i = 0; i<studentList.size(); i++)
-        {
-            if(studentList.get(i).getId() == regNo)
-            {
+    public Student findStudent(int regNo) {
+        for (int i = 0; i < studentList.size(); i++) {
+            if (studentList.get(i).getId() == regNo) {
                 return studentList.get(i);
             }
         }
-        return  null;
+        return null;
     }
 
     // Cleans all the data from the student register
-    public void reset()
-    {
-        studentList = null;
+    public void reset() {
+        studentList.clear();
     }
 
     // Finds all the students that has the given name as a part of their name.
-    public ArrayList<Student> findStudentsByName(String name)
-    {
+    public ArrayList<Student> findStudentsByName(String name) {
+        int StudentListSize = studentList.size();
         ArrayList<Student> students = new ArrayList<Student>();
-        for (int i = 0; i<studentList.size(); i++)
-        {
-            if(studentList.get(i).getFirstName().contains(name))
-            {
-                studentList.add(studentList.get(i));
+        for (int i = 0; i < StudentListSize; i++) {
+            if (studentList.get(i).getFirstName().contains(name)) {
+                students.add(studentList.get(i));
             }
-			
-			
-            if(studentList.get(i).getLastName().contains(name))
-            {
-                studentList.add(studentList.get(i));
+
+            if (studentList.get(i).getLastName().contains(name)) {
+                students.add(studentList.get(i));
             }
         }
         return students;
     }
 
     //Gives all the registration numbers of the students.
-    public ArrayList<Integer> getAllRegistrationNumbers()
-    {
+    public ArrayList<Integer> getAllRegistrationNumbers() {
         ArrayList<Integer> regNumbers = new ArrayList<Integer>();
-        for (Student student: studentList)
-        {
+        for (Student student : studentList) {
             regNumbers.add(student.getId());
         }
-        return  regNumbers;
+        return regNumbers;
     }
 }
